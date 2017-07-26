d80b27b3ccf5a77c4e3fac41a83c191b250bb45b
Jeff Adams <jsmada23@gmail.com>
2017-07-26 09:32:13 -0400

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lionelsite.Startup))]
+namespace Lionelsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
